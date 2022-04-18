85138a4ef32104f1814aa0c841f39c83b527477a
Asad <52581107+Asad-ullah1@users.noreply.github.com>
2022-04-18 19:15:08 +0500

    1st commit

---
+++ add.js
@@ -0,0 +1,74 @@
+                      //Question 1
+/*
+var x =3.45214;
+document.write("number : "+ x +"<br>");
+var a= Math.round(3.4);
+document.write("round off value : " + a +"<br>");
+var b = Math.floor(x);
+document.write("floor value : " + b +"<br>");
+var c =Math.ceil(x);
+document.write("ciel value : " + c +"<br>");
+*/
+
+                      //Question 2
+/*
+                      var x =-2.673;
+                      document.write("number : "+ x +"<br>");
+                      var a= Math.round(x);
+                      document.write("round off value : " + a +"<br>");
+                      var b = Math.floor(x);
+                      document.write("floor value : " + b +"<br>");
+                      var c =Math.ceil(x);
+                      document.write("ciel value : " + c +"<br>");                 
+ */
+
+                           //Question 3 
+                           
+ /*                                                                                                             
+var a= Math.abs(-4);
+var b= Math.abs(5);
+
+document.write("the absolute value of -4 : "+a +"<br>");
+document.write("the absolute value of 5 : "+b);
+*/
+
+                      //Question 4
+ /*
+    var a=Math.floor(Math.random() *6) +1;
+    document.write("value of dice: "+a);                   
+   */
+  
+                       //Question 5                                                                                    
+  /*
+     var head = 1;
+     var tail = 2;
+                      
+     var toss = Math.random() * 2;
+    var floor = Math.floor(toss)
+    if(floor === 1){
+           document.write("2 <br> Random Coin Value: Head")
+       } else if(floor === 0)
+          {
+              document.write("1 <br> Random Coin Value: Tails")
+                 } 
+     */            
+
+                      //Question 6
+ /*                     
+ var a=Math.floor(Math.random() *100) +1;
+ document.write("random value between 1 to 100: "+a); 
+   */                   
+
+                           //Question 7
+                           //Question 8
+ /*                          
+var secret =4;
+var user=prompt("please inter num 1 to 10 " );
+if(secret===+user){
+    document.write("congrates");
+   
+} 
+else {
+    document.write("try again")  
+  }
+*/
